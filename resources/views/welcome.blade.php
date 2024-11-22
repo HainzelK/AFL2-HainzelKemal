7ffb9cda7ae653fb5824a7faa975717488519777
Hainzel <hainzelk@gmail.com>
2024-11-22 13:32:34 +0800

    portfolio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,403 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+  <meta name="copyright" content="MACode ID, https://www.macodeid.com/">
+  
+  <title>Portfolio Hainzel Kemal</title>
+
+  <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/css/themify-icons.css">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+
+  <link rel="stylesheet" type="text/css" href="/assets/vendor/animate/animate.css">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/vendor/owl-carousel/owl.carousel.css">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/vendor/perfect-scrollbar/css/perfect-scrollbar.css">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/vendor/nice-select/css/nice-select.css">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/vendor/fancybox/css/jquery.fancybox.min.css">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/css/virtual.css">
+  
+  <link rel="stylesheet" type="text/css" href="/assets/css/topbar.virtual.css">
+</head>
+<body class="theme-red">
+  
+  <!-- Back to top button -->
+  <div class="btn-back_to_top">
+    <span class="ti-arrow-up"></span>
+  </div>
+  
+  
+  <div class="vg-page page-home" id="home" style="background-image: url(../assets/img/bg_image_1.jpg)">
+    <!-- Navbar -->
+    <div class="navbar navbar-expand-lg navbar-dark sticky" data-offset="500">
+      <div class="container">
+        <a href="" class="navbar-brand">VPortfolio</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#main-navbar" aria-expanded="true">
+          <span class="ti-menu"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navbar">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a href="#home" class="nav-link" data-animate="scrolling">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="#about" class="nav-link" data-animate="scrolling">About</a>
+            </li>
+            <li class="nav-item">
+              <a href="#portfolio" class="nav-link" data-animate="scrolling">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a href="#blog" class="nav-link" data-animate="scrolling">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a href="#contact" class="nav-link" data-animate="scrolling">Contact</a>
+            </li>
+          </ul>
+          <ul class="nav ml-auto">
+            <li class="nav-item">
+              <button class="btn btn-fab btn-theme no-shadow">En</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div> <!-- End Navbar -->
+    <!-- Caption header -->
+    <div class="caption-header text-center wow zoomInDown">
+      <h5 class="fw-normal">Welcome</h5>
+      <h1 class="fw-light mb-4">I'm <b class="fg-theme">Hainzel</b> Kemal</h1>
+      <div class="badge">UI/UX & Web Designer</div>
+    </div> <!-- End Caption header -->
+    <div class="floating-button"><span class="ti-mouse"></span></div>
+  </div>
+  
+  <div class="vg-page page-about" id="about">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-lg-4 py-3">
+          <div class="img-place wow fadeInUp">
+            <img src="../assets/img/work/gambarpfp.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-6 offset-lg-1 wow fadeInRight">
+          <h1 class="fw-light">Hainzel Kemal</h1>
+          <h5 class="fg-theme mb-3">UI/UX & Web Designer</h5>
+          <p class="text-muted">I am a student in Universitas Ciputra Makassar. I'm pursuing an IT Degree. I am born and raised in Makassar City. I love listening to music and read.</p>
+          <ul class="theme-list">
+            <li><b>From:</b> Makassar, Indonesia</li>
+            <li><b>Lives In:</b> Makassar, Indonesia</li>
+            <li><b>Age:</b> 19</li>
+            <li><b>Gender:</b> Male</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container py-5">
+      <h1 class="text-center fw-normal wow fadeIn">My Skills</h1>
+      <div class="row py-3">
+        <div class="col-md-6">
+          <div class="px-lg-3">
+            <h4 class="wow fadeInUp">Coding skills</h4>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">JavaScript</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+              </div>
+            </div>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">PHP</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">60%</div>
+              </div>
+            </div>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">HTML + CSS</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="90">90%</div>
+              </div>
+            </div>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">Python</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">70%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="px-lg-3">
+            <h4 class="wow fadeInUp">Design Skills</h4>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">UI / UX Design</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">85%</div>
+              </div>
+            </div>
+            <div class="progress-wrapper wow fadeInUp">
+              <span class="caption">Web Design</span>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">85%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container pt-5">
+      <div class="row">
+        <div class="col-md-6 wow fadeInRight">
+          <h2 class="fw-normal">Education</h2>
+          <ul class="timeline mt-4 pr-md-5">
+            <li>
+              <div class="title">2023 - Current</div>
+              <div class="details">
+                <h5>College</h5>
+                <small class="fg-theme">Universitas Ciputra Makassar</small>
+                <p>Started college in 2023</p>
+              </div>
+            </li>
+            <li>
+              <div class="title">2020</div>
+              <div class="details">
+                <h5>High School</h5>
+                <small class="fg-theme">SMA Zion Makassar</small>
+                <p>Started high school in 2020 and ended in 2023</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-6 wow fadeInRight" data-wow-delay="200ms">
+          <h2 class="fw-normal">Experience</h2>
+          <ul class="timeline mt-4 pr-md-5">
+            <li>
+              <div class="title">2023 - Current</div>
+              <div class="details">
+                <h5>Junior Code Developer</h5>
+                <small class="fg-theme">Universitas Ciputra Makassar</small>
+                <p>I am trained in Python, HTML, CSS, JS ,Java, Dart, and PHP during my 1 year in college.</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="vg-page page-service">
+    <div class="container">
+      <div class="text-center wow fadeInUp">
+        <div class="badge badge-subhead">Service</div>
+      </div>
+      <h1 class="fw-normal text-center wow fadeInUp">What can i do?</h1>
+      <div class="row mt-5">
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-paint-bucket"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">Web Design</h4>
+              <p>I can do basic web design but nothing too fancy.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-search"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">AI Development</h4>
+              <p>I have made two AI using YoLo v10 and using Tensorflow.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-vector"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">UI/UX Design</h4>
+              <p>I've had my fair share of making UI/UX for my web and application mockup</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+          <div class="card card-service wow fadeInUp">
+            <div class="icon">
+              <span class="ti-desktop"></span>
+            </div>
+            <div class="caption">
+              <h4 class="fg-theme">Web Development</h4>
+              <p>I have made a few projects making a web that can handle a form.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="vg-page page-funfact" style="background-image: url(../assets/img/bg_banner.jpg);">
+    <div class="container">
+      <div class="row section-counter">
+        <div class="col-md-6 col-lg-3 py-4 wow fadeIn">
+          <h1 class="number" data-number="10">10</h1>
+          <span>Clients</span>
+        </div>
+        <div class="col-md-6 col-lg-3 py-4 wow fadeIn">
+          <h1 class="number" data-number="10">10</h1>
+          <span>Project Complete</span>
+        </div>
+        <div class="col-md-6 col-lg-3 py-4 wow fadeIn">
+          <h1 class="number" data-number="3">3  </h1>
+          <span>Project Ongoing</span>
+        </div>
+        <div class="col-md-6 col-lg-3 py-4 wow fadeIn">
+          <h1 class="number" data-number="10">10</h1>
+          <span>Client Satisfaction</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Portfolio page -->
+  <div class="vg-page page-portfolio" id="portfolio">
+    <div class="container">
+      <div class="text-center wow fadeInUp">
+        <div class="badge badge-subhead">Portfolio</div>
+      </div>
+      <h1 class="text-center fw-normal wow fadeInUp">See my work</h1>
+
+      <div class="gridder my-3">
+      <div class="grid-item apps">
+          <a href="https://github.com/HainzelK">
+            <div class="img-place">
+              <img src="../assets/img/work/github.jpg" alt="Mobile Travel App">
+              <div class="img-caption">
+                <h5 class="fg-theme">My Personal Github</h5>
+                <p>Github Profile</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="grid-item apps">
+          <a href="https://www.figma.com/design/q89H01lzH2GQOMRQoEtRdX/Calends?node-id=0-1&node-type=canvas&t=y4QP94Mg5WgX4ZKa-0">
+            <div class="img-place">
+              <img src="../assets/img/work/Calends.jpg" alt="Mobile Travel App">
+              <div class="img-caption">
+                <h5 class="fg-theme">Calends App</h5>
+                <p>First Year Project I made using python</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="grid-item apps">
+          <a href="https://github.com/HainzelK/Siata">
+            <div class="img-place">
+              <img src="../assets/img/work/siata.jpg" alt="Mobile Travel App">
+              <div class="img-caption">
+                <h5 class="fg-theme">Siata Web</h5>
+                <p>Second Year Final Project my team made using HTML, JS, CSS, and Java</p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        </div>
+      </div> <!-- End gridder -->
+
+  </div> <!-- End Portfolio page -->
+  
+    
+  <!-- Footer -->
+  <div class="vg-footer">
+    <h1 class="text-center">Virtual Folio</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 py-3">
+          <div class="footer-info">
+            <p>Where to find me</p>
+            <hr class="divider">
+            <p class="fs-large fg-white">Universitas Ciputra Makassar, Makassar City, Indonesia</p>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3 py-3">
+          <div class="float-lg-right">
+            <p>Follow me</p>
+            <hr class="divider">
+            <ul class="list-unstyled">
+              <li><a href="https://www.instagram.com/hain.zel">Instagram</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3 py-3">
+          <div class="float-lg-right">
+            <p>Contact me</p>
+            <hr class="divider">
+            <ul class="list-unstyled">
+              <li>hainzelk@gmail.com</li>
+              <li>+6282117477752</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center mt-3">
+        <div class="col-12 mb-3">
+          <h3 class="fw-normal text-center">Subscribe</h3>
+        </div>
+        <div class="col-lg-6">
+          <form class="mb-3">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Email address">
+              <input type="submit" class="btn btn-theme no-shadow" value="Subscribe">
+            </div>
+          </form>
+        </div>
+        <div class="col-12">
+          <p class="text-center mb-0 mt-4">Copyright &copy;2020. All right reserved | This template is made with <span class="ti-heart fg-theme-red"></span> by <a href="https://www.macodeid.com/">MACode ID</a></p>
+        </div>
+      </div>
+    </div>
+  </div> <!-- End footer -->
+  
+  
+  <script src="../assets/js/jquery-3.5.1.min.js"></script>
+    
+  <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    
+  <script src="../assets/vendor/owl-carousel/owl.carousel.min.js"></script>
+    
+  <script src="../assets/vendor/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    
+  <script src="../assets/vendor/isotope/isotope.pkgd.min.js"></script>
+    
+  <script src="../assets/vendor/nice-select/js/jquery.nice-select.min.js"></script>
+    
+  <script src="../assets/vendor/fancybox/js/jquery.fancybox.min.js"></script>
+
+  <script src="../assets/vendor/wow/wow.min.js"></script>
+
+  <script src="../assets/vendor/animateNumber/jquery.animateNumber.min.js"></script>
+
+  <script src="../assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+
+  <script src="../assets/js/google-maps.js"></script>
+    
+  <script src="../assets/js/topbar-virtual.js"></script>
+
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIA_zqjFMsJM_sxP9-6Pde5vVCTyJmUHM&callback=initMap"></script>
+  
+</body>
+</html>
